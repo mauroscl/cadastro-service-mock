227806a8005d7a50ab9d875db98ea7e11fb95ed3
Mauro Leal <maurol@dbserver.com.br>
2020-03-25 09:38:45 -0300

    COMMIT INICIAL

---
+++ README.md
@@ -0,0 +1,12 @@
+Clonar a aplicação
+```
+git clone https://github.com/mauroscl/cadastro-service-mock.git
+```
+Instalar dependências
+```
+npm install
+```
+Rodar a aplicação
+```
+node server.js
+``` 
---
+++ server.js
@@ -0,0 +1,54 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.post('/token', (req, res) => {
+    console.log('obtendo token');
+    res.jsonp({
+        "access_token": "21e4a989-c08b-3d9e-b62f-17ee4fd5dc42",
+        "scope": "am_application_scope default",
+        "token_type": "Bearer",
+        "expires_in": 1619
+    });
+})
+
+server.post('/CadastroService/', (req, res) => {
+    console.log('obtendo dias úteis');
+    res.jsonp({
+        "Envelope": {
+            "Body": {
+                "buscarFeriadoResponse": {
+                    "outBuscarFeriado": {
+                        "dias": {
+                            "dia": "2019-03-27T00:00:00-02:00",
+                            "flagDiaUtil": "S"
+                        }
+                    }
+                }
+            }
+        }
+    });
+})
+
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now()
+    }
+    // Continue to JSON Server router
+    next()
+})
+
+// Use default router
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
